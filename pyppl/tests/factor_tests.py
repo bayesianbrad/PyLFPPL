932d22ce6e53cc32b097a1f6e327c33facb421d0
Bradley Gram-Hansen <bradleygramhansen@gmail.com>
2019-03-04 16:11:51 +0000

    updated

--- pyppl/tests/factor_tests.py
+++ pyppl/tests/factor_tests.py
@@ -7,3 +7,32 @@ Date created:  07/01/2019
 
 License: MIT
 '''
+
+from pyppl import compile_model
+from pyppl.utils.core import create_network_graph, display_graph
+
+model_rrhmc_clojure="""
+(let [x (sample (uniform -6 6))
+       absx (max x (- x))
+       A 0.1
+       z (- (sqrt (* x (* A x))))]
+ (if (< (- absx 3) 0)
+     (observe (factor z) 0)
+     (observe (factor (- z 1)) 0 ))
+ x)
+"""
+
+model_rrhmc_python= """
+import torch
+x = sample(uniform(-6,6))
+absx = max(x, -x)
+A = 0.1
+z = -torch.sqrt(x*A*x)
+if absx-3 < 0:
+    observe(factor(z),None)
+    observe(factor(z-1),None)
+"""
+compiled_clojure = compile_model(model_rrhmc_clojure, language='clojure')
+compiled_python = compile_model(model_rrhmc_python, language='python')
+
+print(compiled_python.code)
